c2a004c9d7a09696c6ce5bd7fe55424ea07c47c6
ishar <isharjain19@gmail.com>
2022-09-21 20:49:11 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import inquirer from 'inquirer'
+import { createSpinner } from 'nanospinner'
+import chalk from 'chalk'
+
+console.log(chalk.magenta("Welcome"))
+
+const answers={};
+
+async function askQuestion() {
+    const query = await inquirer.prompt({
+        name: 'question',
+        type: 'input',
+        message: 'Ask your question',
+        default() {
+            return 'How to center a div';
+        },
+    });
+
+    const question = query.question;
+    return question;
+}
+
+async function searchQuestion(question){
+    const spinner = createSpinner('Searching for answer...').start();
+    axios(`https://api.stackexchange.com/2.3/similar?order=desc&sort=relevance&title=${question}&site=stackoverflow`)
+        .then((res)=>{
+            res = res.data.items
+            res.forEach(data => {
+                answers[data.title] = data.link
+            });
+            const titles = Object.keys(answers);
+            titles.forEach((title)=>{
+                console.log('\n')
+                console.log(chalk.bgBlack(chalk.red(title))) 
+                console.log("=>")
+                console.log(chalk.bgBlack(chalk.green(answers[title])))
+            })
+            spinner.success({ text: "These answers are found, click on any link to view the answer" });
+        })
+        .catch((e)=>{
+            spinner.error({ text: "Some unexpected stuff happened, try again" });
+            console.log(e)
+            process.exit(1);
+        })
+}
+const question = await askQuestion();
+searchQuestion(question);
+
+
